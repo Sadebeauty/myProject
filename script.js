08206d799bc4f27b101545bd74cebdff33090eec
Folasade Adesope <adesopefolasade88@gmail.com>
2024-03-16 16:12:56 +0100

    added html, css and JS files

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+ @format
+ */
+/* let person = 'Sade';
+ * let child = "Dayo";
+ * let carNam = "Volvo";
+ * console.log(child);*/
+
+let firstName = "Kolawole";
+let lastName = "Segun";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let a = 2;
+let b = 3;
+let c = a + b;
+console.log("The value of c is " + c + ".");
+
+/*let x = 2 + 3 + "5";
+//the answer will be 55
+console.log(x);
+// if the "5" is the first, it will print everything out as they are i.e 523
+let p = "5" + 2 + 3;
+console.log(p);
+
+//read about modal(partial pages)
+//join twitter community  e.g Software Engineering
+//salesCowboy.co, suffixdesigns.co
+//LOOK UP LINEER GRADIENT ON CSS */
+//you can work on supply chain
+//student result management
+
+
+// conditional Statement
+/*let day = "Friday";
+if ((day = "Friday")) {
+  alert("It's Weekend");
+} else {
+  alert("It's a weekday");
+} */
